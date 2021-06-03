ce321392cb13b8ef33cfa7dddfa0262aac7c0488
shweshipu <shwesh279@gmail.com>
2021-06-03 15:17:09 -0700

    WIP trying to make Attack classes which utilize strategy pattern

---
+++ Attack.java
@@ -0,0 +1,25 @@
+
+public abstract class Attack {
+	protected boolean canAttack;
+	protected int damage;
+	protected double chanceToHit;
+	protected int damageMin, damageMax;
+	
+	// class that calls this object
+	DungeonCharacter attacker;
+	
+	//get our stuff to use, presumably from a Hero extending class
+	Attack(DungeonCharacter attacker, double chanceToHit, int damageMin, int damageMax)
+	{
+		this.attacker = attacker;
+		this.chanceToHit = chanceToHit;
+		this.damageMin = damageMin;
+		this.damageMax = damageMax;
+	}
+	
+	//the attack. return success
+	abstract void doAttack(DungeonCharacter target);
+	
+	
+	
+}
---
+++ DungeonMaster.java
@@ -0,0 +1,11 @@
+
+/*
+ * 
+ * class used to manage the turns, as well as prompt the player for choices.
+ * 
+ * functions as both View and Controller in the MVC pattern?
+ */
+public class DungeonMaster {
+	protected battleChoices()
+	
+}
